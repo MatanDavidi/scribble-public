86cd750c1e7f2075fcfd0dc209f092a6e17b554c
gabrialessi <gabrialessi23@gmail.com>
2019-04-10 22:19:29 +0200

    fix Player - Javadoc and code cleaning

--- src/maquettes/RankingSimulator/src/Player.java
+++ src/maquettes/RankingSimulator/src/Player.java
@@ -1,61 +1,103 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2019 SAMT.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 /**
- * Classe che rappresenta il giocatore.
- * @author Mattia Ruberto
- * @version 09.04.2019
+ * Representation of a player.
+ *
+ * @author mattiaruberto
+ * @author gabrialessi
+ * @version 1.1 (10.04.2019)
  */
 public class Player {
+
     /**
-     * Attributo che rappresenta il nome del giocatore.
+     * Username of the player. Default value: "UNKNOWN".
      */
-    private String username;
+    private String username = "UNKNOWN";
+
     /**
-     * Attributo che rappresenta il punteggio totale del giocatore.
+     * Total score of the player. Default value: 0.
      */
-    private int score;
+    private int score = 0;
+
     /**
-     * Costruttore che inizzializza il giocatore con l'username.
-     * @param username attributo che rappresenta il nome utente del giocatore.
+     * Constructor method where the username is defined.
+     *
+     * @param username Username of the player.
      */
-    public Player(String username){
-        this.username = username;
+    public Player(String username) {
+        setUsername(username);
     }
+
     /**
-     * Costruttore che inizzializza il giocatore con l'username e i punti totali.
-     * @param username attributo che rappresenta il nome utente del giocatore.
-     * @param score attributo che rappresenta il punteggio totale del giocatore.
+     * Constructor method where the username and the total score are defined.
+     *
+     * @param username Username of the player.
+     * @param score Total score of the player.
      */
-    public Player(String username, int score){
-        this.username = username;
-        this.score = score;
+    public Player(String username, int score) {
+        this(username);
+        setScore(score);
     }
+
     /**
-     * Metodo che setta l'username con il valore che gli viene passato.
-     * @param username attributo che rappresenta il nome utente del giocatore.
+     * Get the username of the player.
+     *
+     * @return Username of the player.
      */
-    public void setUsername(String username){
-        this.username = username;
-    }
-    /**
-     * Metodo che ritorna il nome utente del giocatore.
-     * @return il nome utente del giocatore.
-     */
-    public String getUsername(){
+    public String getUsername() {
         return this.username;
     }
+
     /**
-     * Metodo che setta il punteggio del giocatore con il valore che gli viene passato.
-     * @param score attributo che rappresenta il nome utente del giocatore.
+     * Get the total score of the player.
+     *
+     * @return Total score of the player.
      */
-    public void setScore(int score){
-        this.score = score;
-    }
-    /**
-     * Metodo che ritorna il punteggio totale del giocatore.
-     * @return il nome utente del giocatore.
-     */
-    public int getScore(){
+    public int getScore() {
         return this.score;
+    }
+
+    /**
+     * Set the username of the player.
+     *
+     * @param username Username of the player.
+     */
+    private void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * Set the total score of the player.
+     *
+     * @param score Total score of the player.
+     */
+    private void setScore(int score) {
+        if (score < 0) {
+            score = 0;
+        }
+        this.score = score;
     }
 
 }
