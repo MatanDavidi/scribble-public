b26713d4da1273ba61846f5852a2cb1d9ce1babc
MatanDavidi <davidi.matan@gmail.com>
2019-04-10 22:22:07 +0200

    Add license header and javadoc comments

--- src/maquettes/LoginMaquette/src/ChatListener.java
+++ src/maquettes/LoginMaquette/src/ChatListener.java
@@ -1,15 +1,41 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * The MIT License
+ *
+ * Copyright 2019 Matan Davidi.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
 /**
+ * L'interfaccia ChatListener permette di richiamare i metodo utili per gestire
+ * la chat.
  *
  * @author Matan Davidi
+ * @version 2019.04.10
  */
 public interface ChatListener {
-    
+
+    /**
+     * Segnala che deve essere aggiunta alla chat una nuova riga.
+     *
+     * @param newLine La riga da aggiungere alla chat.
+     */
     public void appendToChat(String newLine);
-    
+
 }
