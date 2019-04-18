b0737b507e173766a0793c6fc4c0a5524e1d5e6b
bryanbeffa <bryan.beffa@samtrevano.ch>
2019-04-18 15:30:53 +0200

    updated WordsManager

--- src/maquettes/WordsManager/src/WordManager.java
+++ src/maquettes/WordsManager/src/WordManager.java
@@ -7,91 +7,125 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- *
+ * 
+ * 
  * @author Bryan Beffa
  * @author Matteo Forni
+ * @version 18.04.2019
  */
 public class WordManager {
 
     /**
-     * List that contains all the words of the list.
+     * Lista che contiene tutte le parole del file.
      */
     private List<String> words = new ArrayList<>();
 
     /**
-     * List that contains the picked words from the attribute words.
+     * Lista che contiene le parole già estratte.
      */
     private List<String> pickedWords = new ArrayList<>();
 
+    /**
+     * Attributo booleano che indica se la modalità debug è attiva. Valore di
+     * default = false.
+     */
+    private boolean debug = false;
+
+    /**
+     * Metodo costruttore che richiede la lista di parole del file.
+     *
+     * @param words lista di parole del file.
+     */
     public WordManager(List<String> words) {
         this.words = words;
     }
 
     /**
-     * Method that returns a random line from the list.
+     * Metodo costruttore che richiede la lista di parole del file e se la
+     * modalità debug è attiva o meno.
      *
-     * @return a random line of the list.
+     * @param words lista di parole del file.
+     * @param debug parametro che indica se la modalità debug è attiva.
+     */
+    public WordManager(List<String> words, boolean debug) {
+        this.words = words;
+        this.debug = debug;
+    }
+
+    /**
+     * Metodo che ritorna una parola casuale dalla lista delle parole non ancora
+     * estratte.
      */
     private String getRandomWord() {
-        //random number
+        //numero casuale
         int rnd = (int) (Math.random() * words.size());
 
         return words.get(rnd);
     }
 
     /**
-     * Method that sorts alphabetically the list of the picked words.
+     * Metodo che ordina la lista di parole estratte in ordine alfabetico.
      */
     public void sortList() {
         Collections.sort(pickedWords);
 
-        for (String word : pickedWords) {
-            System.out.println("Word: " + word);
+        if (debug) {
+            for (String word : pickedWords) {
+                System.out.println("Word: " + word);
+            }
         }
     }
 
+    /**
+     * Metodo che ritorna una parola casuale non ancora estratta.
+     * 
+     * @return parola casuale non ancora estratta.
+     */
     public String getUniqueNewWord() {
+        //controllo se la lista di parole non estratte è vuota
         if (words.size() > 0) {
             String word = getRandomWord();
 
             words.remove(word);
             pickedWords.add(word);
-            
+
             return word;
         }
         
         words = pickedWords;
         pickedWords = new ArrayList<>();
 
-        System.out.println("Parole terminate: ricarico la lista");
-        
-        //recursive call
+        if (debug) {
+            System.out.println("Parole terminate: ricarico la lista");
+        }
+
+        //chiamata recursiva
         return getUniqueNewWord();
     }
 
     public static void main(String[] args) {
 
-        //create the file path
+        //creo il percorso del file
         Path path = Paths.get("data", "list.txt");
         FileManager fManager;
         WordManager wManager;
 
         try {
-            //create a new FileManager
+            //creo il file manager
             fManager = new FileManager(path);
             List<String> wordsList = fManager.getWords();
 
-            //create a new WordManager
-            wManager = new WordManager(wordsList);
+            //creo il WordManager
+            wManager = new WordManager(wordsList, true);
 
             System.out.println("Lista parole casuali");
             for (int i = 0; i < 1000; i++) {
-                //get a word never picked
+                //ottengo una parola non ancora estratta
                 System.out.println(i + ". Parola scelta: "
                         + wManager.getUniqueNewWord());
             }
 
-            //sort list
+            //ordino la lista
             wManager.sortList();
 
         } catch (IOException ex) {
