f706b9a4eed33316543147329cdd27fffcc17b41
Matan Davidi <davidi.matan@gmail.com>
2019-05-07 17:37:41 +0200

    Add PlayerRole enumeration

---
+++ src/project/Scribble/src/samt/scribble/client/game/PlayerRole.java
@@ -0,0 +1,45 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2019 Matan Davidi.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package samt.scribble.client.game;
+
+/**
+ * Enumerazione contenente i ruoli che è possibile assumere da giocatore.
+ *
+ * @author Matan Davidi
+ * @version 1.0 (2019-05-07 - 2019-05-07)
+ */
+public enum PlayerRole {
+
+    /**
+     * Indica il ruolo del giocatore che deve disegnare e far indovinare agli
+     * altri utenti la parola che sta disegnando.
+     */
+    Drawer,
+    /**
+     * Indica il ruolo di uno dei giocatori che deve indovinare la parola che
+     * sta disegnando il Drawer.
+     */
+    Guesser
+
+}
