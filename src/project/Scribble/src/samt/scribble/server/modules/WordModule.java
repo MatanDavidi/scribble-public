993b34873509e518c017568bb1b517baf10cf6c9
PaoloGuebeli <37575527+PaoloGuebeli@users.noreply.github.com>
2019-05-08 18:07:33 +0200

    Added guessing managing class

---
+++ src/project/Scribble/src/samt/scribble/server/modules/WordModule.java
@@ -0,0 +1,71 @@
+
+/*
+ * The MIT License
+ *
+ * Copyright 2019 guebe.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package samt.scribble.server.modules;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import samt.scribble.DefaultScribbleParameters;
+import samt.scribble.communication.messages.EchoMessage;
+import samt.scribble.communication.messages.Message;
+import samt.scribble.communication.messages.WordGuessMessage;
+import samt.scribble.communication.messages.WordGuessedMessage;
+import samt.scribble.wordmanager.WordManager;
+
+/**
+ * Questo metodo legge la parola inviata e controlla se questa è corretta,
+ * se lo è controlla quanti player devono ancora indovinare.
+ * 
+ * @author Paolo Guebeli
+ * @version 1.0 (2019-05-08)
+ */
+public class WordModule {
+    
+    public static DatagramPacket guessed(DatagramPacket datagramPacket) throws IOException{
+        WordManager wordManager = new WordManager(DefaultScribbleParameters.WORDS_CSV_PATH);
+        String word = "";
+        for (int i = 1; i < datagramPacket.getData().length; i++) {
+            if (datagramPacket.getData()[i] != 0) {
+                word += (char) datagramPacket.getData()[i];
+            }
+        }
+        
+        Message message;
+        
+        if(wordManager.isGuessedWord(word)){
+            message = new WordGuessedMessage(datagramPacket.getAddress().toString());
+        }else{
+            message = new WordGuessMessage(word);
+        }
+        
+        return new DatagramPacket(
+                message.getWholeMessage(),
+                message.getWholeMessage().length,
+                datagramPacket.getAddress(),
+                DefaultScribbleParameters.DEFAULT_GROUP_PORT
+        );
+    }
+    
+}
