344fdce8e4f95b18e5be7f2560dfc3276109b3b2
PaoloGuebeli <37575527+PaoloGuebeli@users.noreply.github.com>
2019-05-08 18:06:58 +0200

    Added WordManager to main Project

---
+++ src/project/Scribble/src/samt/scribble/wordmanager/TestWord.java
@@ -0,0 +1,33 @@
+package samt.scribble.wordmanager;
+
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ *
+ * @author Bryan
+ */
+public class TestWord {
+    public static void main(String[] args) {
+
+        //creo il percorso del file
+        Path path = Paths.get("data", "test.txt");
+        WordManager wManager;
+
+        try {
+
+            //creo il WordManager
+            wManager = new WordManager(path, true);
+
+            //estraggo la parola
+            wManager.getUniqueNewWord();
+            wManager.isGuessedWord("albero");
+
+        } catch (IOException ex) {
+            System.out.println("Error: " + ex.getMessage());
+        }
+
+    }
+}
