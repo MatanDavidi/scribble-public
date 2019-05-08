6c3203b4eae603e725dd85a117d7caceee406468
PaoloGuebeli <37575527+PaoloGuebeli@users.noreply.github.com>
2019-05-08 22:25:18 +0200

    Removed word guessed

--- src/project/Scribble/src/samt/scribble/communication/Commands.java
+++ src/project/Scribble/src/samt/scribble/communication/Commands.java
@@ -15,7 +15,7 @@ public class Commands {
     /**
      * Il numero totale di byte comando definiti.
      */
-    public final static short COMMANDS_NUMBER = 14;
+    public final static short COMMANDS_NUMBER = 13;
 
     /**
      * Un byte di comando che segnala che il messaggio inviato è di tipo
@@ -95,9 +95,4 @@ public class Commands {
      * parola è stata indovinata.
      */
     public final static byte GUESSED_WORD = 12;
-
-    /**
-     * Un byte di comando che segnala che il messagio e una parola indovinata.
-     */
-    public final static byte WORD_GUESSED = 13;
 }
