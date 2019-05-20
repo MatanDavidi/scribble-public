5a5747d52df0aaf3100edfcc2ea4adb686987825
Matan Davidi <davidi.matan@gmail.com>
2019-05-20 14:04:38 +0200

    Rename endGame package to endgame

--- src/project/Scribble/src/samt/scribble/client/endgame/EndGameListener.java
+++ src/project/Scribble/src/samt/scribble/client/endgame/EndGameListener.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package samt.scribble.client.endGame;
+package samt.scribble.client.endgame;
 
 /**
  *
--- src/project/Scribble/src/samt/scribble/client/endgame/EndPanel.java
+++ src/project/Scribble/src/samt/scribble/client/endgame/EndPanel.java
@@ -21,9 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package samt.scribble.client.endGame;
-
-import samt.scribble.client.welcome.WelcomeListener;
+package samt.scribble.client.endgame;
 
 /**
  *
